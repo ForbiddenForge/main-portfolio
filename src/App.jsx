b0a11b20e300adaceb2a27bcc7fca3af97d815af
Peter White <119447237+ForbiddenForge@users.noreply.github.com>
2023-12-06 16:19:02 -0600

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,15 @@ import { Route, BrowserRouter as Router, Routes} from 'react-router-dom'
 import Navbar from './components/Navbar'
 import { Home, About, Projects, Contact }  from './pages'
 
+
 const App = () => {
   return (
-    <main className='bg-slate-300/20'>
+    <main className='bg-gradient-to-t from-white to-red-100 h-full'>
         <Router>
             <Navbar />
             <Routes> 
                 <Route path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
+                <Route path="/about" element={<About />}  />
                 <Route path="/projects" element={<Projects />} />
                 <Route path="/contact" element={<Contact />} />
 
